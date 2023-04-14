eb2d399b7515fbce33cc12878fb611ac881775d2
santty04 <sarango844@misena.edu.co>
2023-04-14 16:49:01 -0500

    Avance en consumo de api de peliculas, ya se obtiene tendencias, busqueda, y categorias

--- src/main.js
+++ src/main.js
@@ -1,3 +1,58 @@
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+  params: {
+    api_key: API_KEY,
+  },
+});
+
+async function getTrendingMoviesPreview() {
+  const { data } = await api("trending/movie/day");
+  const movies = data.results;
+
+  trendingMoviesPreviewList.innerHTML = '';
+  movies.forEach((movie) => {
+   
+
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container");
+
+    const movieImg = document.createElement("img");
+    movieImg.classList.add("movie-img");
+    movieImg.setAttribute("alt", movie.title);
+    movieImg.setAttribute(
+      "src",
+      "https://image.tmdb.org/t/p/w300" + movie.poster_path
+    );
+
+    movieContainer.appendChild(movieImg);
+    trendingMoviesPreviewList.appendChild(movieContainer);
+  });
+}
+
+async function getCategoriesPreview() {
+  const { data } = await api("genre/movie/list");
+
+  const categories = data.genres;
+ 
+  categoriesPreviewList.innerHTML = '';
+  categories.forEach((category) => {
 
 
-console.log('Hello, world');
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add("category-container");
+
+    const categoryTitle = document.createElement("h3");
+    categoryTitle.classList.add("category-title");
+    categoryTitle.setAttribute("id", "id" + category.id);
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    categoriesPreviewList.appendChild(categoryContainer);
+  });
+}
+
+
